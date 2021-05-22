c32345071bdfd27ed59750707a0051627b3537ab
ohr486 <ohr486@gmail.com>
2021-05-23 01:46:35 +0900

    update terraform ver

--- aws/backend.tf
+++ aws/backend.tf
@@ -5,6 +5,6 @@ terraform {
     region                  = "ap-northeast-1"
     dynamodb_table          = "tfstate"                   # SET YOUR DDB TABLE
     shared_credentials_file = "~/.aws/credentials"
-    profile                 = "blog-ohr486-net-terraform" # SET YOUR PROFILEt
+    profile                 = "blog-ohr486-net-terraform" # SET YOUR PROFILE
   }
 }
