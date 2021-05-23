d756f3e62f7ee605b6fb1f2433abe61409dc71b0
ohr486 <ohr486@gmail.com>
2021-05-23 22:29:25 +0900

    add s3 policy

--- aws/s3.tf
+++ aws/s3.tf
@@ -1,4 +1,31 @@
 resource "aws_s3_bucket" "blog_ohr486_net" {
   bucket = "blog.ohr486.net"
-  acl = "private"
+  acl = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "blog_ohr486_net" {
+  bucket = aws_s3_bucket.blog_ohr486_net.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "BLOG_OHR486_NET_POLICY"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = [
+          "s3:GetObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::blog.ohr486.net/*"
+        ]
+      }
+    ]
+  })
 }
