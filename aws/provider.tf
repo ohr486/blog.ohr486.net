be76034cffa0be615308c1d538e02518191c6aca
ohr486 <ohr486@gmail.com>
2021-05-23 21:56:27 +0900

    add s3

---
+++ aws/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region = "ap-northeast-1"
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "blog-ohr486-net-terraform" # SET YOUR PROFILE
+}
---
+++ aws/s3.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "blog_ohr486_net" {
+  bucket = "blog.ohr486.net"
+  acl = "private"
+}
