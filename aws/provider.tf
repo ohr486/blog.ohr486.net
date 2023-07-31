e26239a933dc0059953c84bd7249ffebea36322b
ohr486 <ohr486@gmail.com>
2023-07-31 23:28:13 +0900

    update tf

--- aws/provider.tf
+++ aws/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-northeast-1"
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = "blog-ohr486-net-terraform" # SET YOUR PROFILE
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "blog-ohr486-net-terraform" # SET YOUR PROFILE
 }
